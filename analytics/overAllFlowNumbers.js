285062b529ee9d373b109a5c971889caaf5e46b0
madhuri <madhuri@openbrix.co.uk>
2022-10-28 15:12:11 +0530

    que.6 to 18

--- analytics/overAllFlowNumbers.js
+++ analytics/overAllFlowNumbers.js
@@ -8,11 +8,123 @@ function lengthOfFlows(flows) {
         return flow.is_deleted
     })
     return deletedFlows.length
+}
 
+function lengthOfCompletedFlows(flows) {
+    let completedFlows = flows.filter(flow => {
+        return flow.is_completed
+    })
+    return completedFlows.length
+}
+
+function lengthOfArchivedFlows(flows) {
+    let archivedFlows = flows.filter(flow => {
+        return flow.is_archived
+    })
+    return archivedFlows.length
+}
+
+function flowsWithMoreThan2Tenants (flows) {
+    let numberOfTenants = flows.filter(flow => {
+        return flow.tenants.length > 2
+    })
+    return numberOfTenants.length
+}
+
+function flowsWithTenatsAppUserId(flows) {
+    let tenants = flows.filter(flow => {
+        let tenantsWithAppUser = flow.tenants.filter(user => {
+            return user.app_user.user.is_delete
+        })
+    })
+    return tenants.length
+}
+
+
+
+function numberOfAstGenerated(flows) {
+    let astGenerated = flows.filter(flow =>{
+        return flow.is_ast_generated
+    })
+    return astGenerated.length
+}
+
+function numberOfNhaGenrated(flows) {
+    let nhaGenerated = flows.filter(flow => {
+        return flow.is_nha_generated
+    })
+    return nhaGenerated.length
+}
+
+function numberOfFlowsUsed(flows) {
+    let flowsUsed = flows.filter(flow => {
+        return flow.is_flow_used
+    })
+    return flowsUsed.length
+}
+
+function numberOfReapitTenancyId(flows) {
+    let reapitTenancyId = flows.filter(flow => {
+        return flow.reapit_tenancy_id
+    })
+    return reapitTenancyId.length 
+}
+
+function numberOfIsAddedToProperty(flows) {
+    let addedToProperty = flows.filter(flow => {
+        return flow.is_added_to_property
+    })
+    return addedToProperty.length 
+}
+
+function numberOfEmptyLandlords(flows) {
+    let landlords = flows.filter(flow => {
+        return flow.landlord ==0
+    })
+    return landlords.length
+}
+
+function numberOfAstgenerated(flows) {
+    let astGenerated = flows.filter(flow => {
+        return flow.is_ast_signed !== 'false'
+    })
+    return astGenerated.length
+}
+
+function numberOfAstIsNotGenerated(flows) {
+    let astNotGenerated = flows.filter(flow => {
+        return flow.is_ast_signed =='false'
+    })
+    return astNotGenerated.length
+}
+
+function OpenFlowRentalOffer(flow) {
+    let rentalOffer = flow.filter(flow =>{
+        let rentalAmount = flow.open_flow_rental_offer.filter(offer => {
+            return offer.rental_amount >5000
+        })
+        
+    })
+    return rentalOffer.length
 }
 
 
 module.exports = {
     lengthOfFlows, 
-    lengthOfDeletedFlows
+    lengthOfDeletedFlows,
+    lengthOfCompletedFlows,
+    lengthOfArchivedFlows,
+    numberOfAstGenerated,
+    numberOfNhaGenrated,
+    numberOfFlowsUsed,
+    numberOfReapitTenancyId,
+    numberOfIsAddedToProperty,
+    flowsWithMoreThan2Tenants,
+    flowsWithTenatsAppUserId,
+    numberOfEmptyLandlords,
+    numberOfAstgenerated,
+    numberOfAstIsNotGenerated,
+    OpenFlowRentalOffer
+
+
 }
--- run.js
+++ run.js
@@ -7,6 +7,41 @@ console.log("Total Flows  ",length)
 let length1 = flowNumbers.lengthOfDeletedFlows(flows.flows)
 console.log("Length of deleted flows  ", length1)
 
+let length2 = flowNumbers.lengthOfCompletedFlows(flows.flows)
+console.log("Length of completed flows ", length2)
 
+let length3 = flowNumbers.lengthOfArchivedFlows(flows.flows)
+console.log("length of archived flows ", length3)
 
+let length4 = flowNumbers.flowsWithMoreThan2Tenants(flows.flows)
+console.log("Numbers of Tenants greater than 2 ", length4)
 
+let lengthof = flowNumbers.flowsWithTenatsAppUserId(flows.flows)
+console.log("number of is deleted in user ", lengthof)
+
+let length5 = flowNumbers.numberOfAstGenerated(flows.flows)
+console.log("length of ast generated ", length5)
+
+let length6 = flowNumbers.numberOfNhaGenrated(flows.flows)
+console.log("length of nha generated ", length6)
+
+let length7 = flowNumbers.numberOfFlowsUsed(flows.flows)
+console.log("length of flow used ", length7)
+
+let length8 = flowNumbers.numberOfReapitTenancyId(flows.flows)
+console.log("Number of reapit tenancy id ", length8)
+
+let length9 = flowNumbers.numberOfIsAddedToProperty(flows.flows)
+console.log("Number of is added to property ", length9)
+
+let length10 = flowNumbers.numberOfEmptyLandlords(flows.flows)
+console.log("Number of empty landlords ", length10)
+
+let length11 = flowNumbers.numberOfAstgenerated(flows.flows)
+console.log("Number of flows where ast generated is assigned ", length11)
+
+let length12 = flowNumbers.numberOfAstIsNotGenerated(flows.flows)
+console.log("Number of flows where ast generated is not assigned ", length12)
+
+let length13 = flowNumbers.OpenFlowRentalOffer(flows.flows)
+console.log("Number of flow of open flow rental offer >5000 ", length13)
